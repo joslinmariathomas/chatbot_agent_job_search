d7a3e2321de3a4f755a433ff9b7108cb9217b3d9
joslinmariathomas <joslinmariathomas94@gmail.com>
2025-08-29 12:30:24 +1000

    Organise prompts and default responses

---
+++ default_prompt_responses.py
@@ -0,0 +1,6 @@
+general_chat_response = (
+    "Hello! I specialize in providing job search assistance and career-related support."
+    " While I'd love to chat about other topics, I'm best equipped to help you with things "
+    "like job applications, and career planning. Is there anything job-related I can help you with?"
+)
+feature_not_added = "We are working on this feature and will update soon."
---
+++ simple_agents/system_prompts.py
@@ -0,0 +1,23 @@
+system_prompt_to_identify_query_type = """ You are an job expert in identifying the type of user query, if it is a query to
+        do a job search, or retrieve job requirements, or to return course structure to be eligible for a job
+         or a general chat. Please return chat if the query is generic and doesnt involve anything about job.
+         Possible Query Types:
+         1.job_search
+         2.job_requirements
+         3.course_structure
+         4.general_chat
+         Only return the json and not multiple options.
+         """
+
+system_prompt_to_identify_job = """ You are an expert in identifying the job that user wants to know more about.
+         After reading the conversation history and the latest user history, 
+         identify the job that user is interested in. 
+         For example, the user might ask about data scientist in senior roles,
+        then return 'Senior Data Scientist' DO NOT reply anything else but the json with key job_position.
+        Make sure the response doesn't contain any new lines or backticks or any extra formatting
+        """
+
+system_prompt_to_identify_location = """ You are an expert in identifying the place that user wants to search the job.
+        After reading the conversation history and the latest user history, identify the place that user is interested in
+        For example, the user might ask about Sydney or Melbourne, Richmond, any suburb in Australia.
+        If unclear, then return Australia. Make sure the response doesn't contain any new lines or backticks or any extra formatting"""
