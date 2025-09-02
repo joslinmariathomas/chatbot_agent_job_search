50abb3a4304f86420bcb413a183f020ef5d20770
joslinmariathomas <joslinmariathomas94@gmail.com>
2025-09-02 17:45:04 +1000

    parse resume

--- utils/resume_extractor/resume_parser.py
+++ utils/resume_extractor/resume_parser.py
@@ -7,13 +7,16 @@ from docling.document_converter import DocumentConverter
 class CVParser:
     def __init__(self):
         self.converter = DocumentConverter()
+        self.parsed_uploaded_resume = False
+        self.resume_in_text = None
 
     def parse_resume(self, resume_pdf: BinaryIO):
+        """Parses the resume in text format but the syntax would be markdown"""
         with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp_file:
             tmp_file.write(resume_pdf.read())
             tmp_path = tmp_file.name
 
         parsed_resume = self.converter.convert(tmp_path).document
-        resume_in_text = parsed_resume.export_to_text()
+        self.resume_in_text = parsed_resume.export_to_text()
         logging.info("Resume processed and ready for job matching!")
-        return resume_in_text
+        self.parsed_uploaded_resume = True
